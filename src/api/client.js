582de7dcb130656ab3d89fdc22b599b19b276d9a
Jezer Rázuri <jezerrazuri@upeu.edu.pe>
2023-07-04 18:37:18 -0500

    first commit

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,8 @@
 import axios from "axios";
-export const testDomain = `localhost:5000`;
-export const productionDomain = `https://wine-api.gns3-vm.tk`;
-export const baseURL = `http://${productionDomain}`;
+export const domain = `wine-api.gns3-vm.tk`;
+export const testDomain = `http://localhost:5000`;
+export const productionDomain = `https://${domain}`;
+export const baseURL = `${productionDomain}`;
 
 export const client = axios.create({
   baseURL,
